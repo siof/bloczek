198043a9591fc6d7bd2ab9741290298ce907ddfa
lkoziel <lkoziel@localhost>
2009-11-17 12:27:39 +0100

    brakujace pliki linii

---
+++ lib/Bloki/Linia.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace libbloki
+{
+    public partial class Linia : UserControl
+    {
+        private Point klikoffset;
+        private bool klik;
+        Graphics graph;
+
+        public Linia()
+        {
+            InitializeComponent();
+            graph = CreateGraphics();
+        }
+
+        private void Linia_MouseDown(object sender, MouseEventArgs e)
+        {
+            klik = true;
+            klikoffset.X = e.X;
+            klikoffset.Y = e.Y;
+        }
+
+        private void Linia_MouseUp(object sender, MouseEventArgs e)
+        {
+            klik = false;
+        }
+
+        private void Linia_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (klik)
+            {
+                this.Left = e.X + this.Left - klikoffset.X;
+                this.Top = e.Y + this.Top - klikoffset.Y;
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+           
+            Graphics graph = pe.Graphics;
+            Pen pn = new Pen(Color.Black, 1);
+            Point[] p = new Point[2];
+            p[0].X = 2; p[0].Y = 2;
+            p[1].X = 134; p[1].Y = 2;
+            pn.EndCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
+
+            graph.DrawLine(pn,p[0],p[1]);
+        }
+
+
+    }
+}
