3a6ad5b268cc401145363890d6ba6ac6666965c8
siof <siof@localhost>
2009-12-26 15:16:42 +0100

    dodanie obslugi tablic w opcji czytania bloku We/Wy

--- lib/Bloki/Czytaj.cs
+++ lib/Bloki/Czytaj.cs
@@ -13,22 +13,28 @@ namespace libbloki
     {
         IList<Zmienna> listaZmiennych;
         int index;
+        int nrElTab;
 
         public Czytaj()
         {
             InitializeComponent();
         }
 
-        public Czytaj(IList<Zmienna> usr, int ind)
+        public Czytaj(IList<Zmienna> usr, int ind, int nrEl)
         {
             InitializeComponent();
             listaZmiennych = usr;
             this.index = ind;
+            nrElTab = nrEl;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            listaZmiennych[index].wartosc = maskedTextBox1.Text.ToString();
+            if (listaZmiennych[index].tablica == true)
+                listaZmiennych[index].wartosci[nrElTab] = maskedTextBox1.Text.ToString();
+            else
+                listaZmiennych[index].wartosc = maskedTextBox1.Text.ToString();
+
             Close();
         }
     }
